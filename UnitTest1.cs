a88ee4b0ce0738934d0ffd539531f8b565686930
ramnaresh-git <71754198+ramnaresh-git@users.noreply.github.com>
2020-09-23 10:55:16 -0700

    Commit

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -11,5 +11,11 @@ namespace MyFirstUnitTestProject1
             Assert.IsTrue(true);
         }
 
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(true);
+        }
+
     }
 }
