aa84a9e82f8d010a25cce21bf5a1f4d8c5810e77
ramnaresh-git <71754198+ramnaresh-git@users.noreply.github.com>
2020-09-24 01:05:05 -0700

    Revert "Commit"
    This reverts commit 47500512a35a68d1067d2edbd04985204cfce7ec.

---
+++ UnitTest2.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyFirstUnitTestProject1
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod21()
+        {
+            Assert.IsTrue(true);
+        }
+
+
+        [TestMethod]
+        public void TestMethod22()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod23()
+        {
+            Assert.IsTrue(true);
+        }
+
+    }
+}
