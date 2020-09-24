839281c206ee41b20f071e446f685d19bc265b91
ramnaresh-git <71754198+ramnaresh-git@users.noreply.github.com>
2020-09-24 01:07:49 -0700

    Adding class 3

---
+++ UnitTest3.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyFirstUnitTestProject1
+{
+    [TestClass]
+    public class UnitTest3
+    {
+        [TestMethod]
+        public void TestMethod31()
+        {
+            Assert.IsTrue(true);
+        }
+
+    }
+}
